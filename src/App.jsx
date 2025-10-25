751a5246f55a63c3cfab782c61ad49e6749e50cc
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-10-25 01:23:50 -0500

    Toggle Text

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import { useState } from "react";
 function myReact() {
   const [count, setCount] = useState(0)
   // setTimeout(() => {alert("This message appears after 10 seconds")}, 10000);
-  return <>
-  <p>The count is {count}</p>
-  <button onClick={() => {setCount(count+1)}}>Increase</button>
-  <button onClick={() => {setCount(count-1)}}>Decrease</button>
-  <button onClick={() => {setCount(0)}}>Reset</button>
-  </>
+  const [show, setShow] = useState(false)
+
+
+  return <div className="main">
+    <div className="counter">
+      <p>The count is {count}</p>
+      <button onClick={() => {setCount(count+1)}}>Increase</button>
+      <button onClick={() => {setCount(count-1)}}>Decrease</button>
+      <button onClick={() => {setCount(0)}}>Reset</button>
+    </div>
+    <br />
+
+
+    <div className="toggleText">
+      {show && <p>Hello!!</p>}
+      <button onClick={() => {setShow(!show)}}>{show ? "Hide" : "Show"}</button>
+    </div>
+  </div>
 }
 
-export default myReact
+
+
+export default myReact;
