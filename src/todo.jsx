434b9e8c7b5294a9be3bd002dcea0b797bc28064
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-10-25 02:42:52 -0500

    Done

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,27 +1,53 @@
 import { useState } from "react";
 
 function ToDoList() {
-    const [task, setTask] = useState("");  // state for task
-    const [list, setList] = useState([]);  // state for list of tasks
+  const [task, setTask] = useState(""); // state for task
+  const [list, setList] = useState([]); // state for list of tasks
+  const [num, setNum] = useState(0);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-    const addTask = () => {
-        if (task.trim() === "")
-            return;
-        setList([...list, task]);
-        setTask("");
-    }
+  const addTask = () => {
+    if (task.trim() === "") return;
+    setList([...list, task]);
+    setTask("");
+  };
 
-    return <div className="todo-container">
-        <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+  const generateNum = () => {
+    setNum(Math.floor(Math.random() * 100)) // generates random number from 0-100
+    // Math.random() generated decimal number between 0-1
+  }
+
+  return (
+    <div className="main">
+        <div className="randomNum">
+            <button onClick={generateNum}>Generate Random Number</button>
+            <p>The random number is {num}</p>
+        </div>
+        <br />
+
+      <div className="todo-container">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
         <button onClick={() => addTask()}>Add Task</button>
         <br />
         <h3>Your Tasks:</h3>
         <ul>
-            {list.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
+          {list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ul>
+      </div>
+      <br />
+
+      <div className="time">
+        <p>Current time: {time}</p>
+        <button onClick={() => {setTime(new Date().toLocaleTimeString())}}>Refresh Time</button>
+      </div>
     </div>
+  );
 }
 
 export default ToDoList;
