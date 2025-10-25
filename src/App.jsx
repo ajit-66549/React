91f56190586012907c683d4908184d57ef6d8c73
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-10-25 01:34:10 -0500

    practicing more useState examples

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function myReact() {
   const [count, setCount] = useState(0)
   // setTimeout(() => {alert("This message appears after 10 seconds")}, 10000);
   const [show, setShow] = useState(false)
+  const [color, setColor] = useState("green")
+  const [like, setLike] = useState(false)
 
 
   return <div className="main">
@@ -15,10 +17,22 @@ function myReact() {
     </div>
     <br />
 
-
     <div className="toggleText">
       {show && <p>Hello!!</p>}
       <button onClick={() => {setShow(!show)}}>{show ? "Hide" : "Show"}</button>
+    </div>
+    <br />
+
+    <div className="bgColor" style={{ backgroundColor: color, height: "50vh" }}>
+      <button onClick={() => {setColor("Blue")}}>Blue</button>
+      <button onClick={() => {setColor("lightgreen")}}>Light Green</button>
+    </div>
+    <br />
+
+    <div className="likeToogle">
+      <button onClick={() => {setLike(!like)}}>
+        {like ? "💖 Liked" : "🤍 Like"}
+      </button>
     </div>
   </div>
 }
